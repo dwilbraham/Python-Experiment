a8c6e6585d744a7b722cfe30c286a42ffe267e10
Dan Wilbraham <dwilbraham+git@gmail.com>
2011-09-18 23:18:08 +0100

    Add a sample test file and outline script for updating a version number.

---
+++ VersionNumberUpdate/test.spec
@@ -0,0 +1,4 @@
+#Test file to increment version number
+Version: 0.0.1
+Release: 1
+Test: 1.2.3
---
+++ VersionNumberUpdate/updateVersionNumber.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+#Script to take a file and increment any of the fields in the version number.
+
+print 'test'
