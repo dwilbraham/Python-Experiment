b27f4f4fe4ecb874dd19c55a45d3d3f21b7adc2a
Dan Wilbraham <dwilbraham+git@gmail.com>
2011-09-21 23:26:39 +0100

    Update updateVersionNumber to use optparse for command line options.

--- VersionNumberUpdate/updateVersionNumber.py
+++ VersionNumberUpdate/updateVersionNumber.py
@@ -3,8 +3,14 @@
 #Script to take a file and increment any of the fields in the version number.
 
 import sys
+from optparse import OptionParser
 
 if __name__ == "__main__":
-    print 'test'
-    for file in sys.argv[1:]:
+    usage = "usage: %prog [options] SPEC_FILE..."
+    parser = OptionParser(usage)
+    parser.add_option("-r", "--release", action="store_true", help="Increment release number")
+    parser.add_option("-v", "--version", dest="version", type="int", metavar="FIELD", help="Increment version number FIELD")
+    (options, args) = parser.parse_args()
+    print "Version = " + str(options.version)
+    for file in args:
         print file,
